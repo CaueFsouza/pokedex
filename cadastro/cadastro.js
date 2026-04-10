c07f2ceb9065afff3b8639bbc29a6468d9ef53a3
CaueFsouza <caueferreiradesouza760@gmail.com>
2026-04-10 02:45:25 -0300

    botoes e js

---
+++ cadastro/cadastro.js
@@ -0,0 +1,32 @@
+function cadastro (){
+
+    let usuario = document.getElementById("usuario").value;
+    let senha = document.getElementById("senha").value;
+
+    if(usuario === "" || senha === ""){
+        alert("preencha o campo com informações!")
+        return;
+    }
+
+    localStorage.setItem("usuario", usuario);
+    localStorage.setItem("senha", senha);
+
+    alert("cadastro realizado!");
+    window.location.href = "login.html"
+}
+
+function login (){
+    let usuario = document.getElementById("usuariologin").value;
+    let senha = document.getElementById("senhalogin").value;
+
+    let usuariosalvo = localStorage.getItem("usuario");
+    let senhasalva = localStorage.getItem("senha");
+
+    if(usuario === usuariosalvo && senha === senhasalva){    
+        alert("Login efetuado com Sucesso!")
+        window.location.href = "../prototipo/pokemons.html"
+    } else {
+        alert("Login invalido!")
+    }
+    
+}
